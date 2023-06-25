ba88b4a4da3d00940f4f317fe9a54078cbba632b
Karl Johann Lattikas <lattikaskarljohann@gmail.com>
2023-06-25 20:11:19 +0300

    test using InvoicesByProvider view in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,49 @@
-function App() {
+import {useState, useEffect} from "react";
+import axios from "axios";
 
-  return (
-    <h1>Hello!</h1>
-  )
+function App() {
+    const [providers, setProviders] = useState([])
+    const [invoices, setInvoices] = useState([])
+
+    useEffect(
+        () => {
+            axios.get('http://127.0.0.1:8000/providers/')
+                .then(response => {
+                    setProviders(response.data)
+                })
+        }, []
+    )
+
+    const handleChange = (event) => {
+        const index = event.target.value
+        if (index !== '') {
+            axios.get(`http://127.0.0.1:8000/providers/${index}/invoices/`)
+                .then(response => {
+                    setInvoices(response.data)
+                })
+        } else {
+            setInvoices([])
+        }
+    }
+
+    return (
+        <>
+            <select onChange={handleChange}>
+                <option value=''>Select an option</option>
+                {providers.map((provider, index) => (
+                    <option key={index} value={provider.id}>{provider.name}</option>
+                ))}
+            </select>
+            <br/>
+            {invoices.map((invoice, index) => (
+                <ul key={index}>
+                    <li>{invoice.document_number}</li>
+                    <li>{invoice.created}</li>
+                    <li>{invoice.due_date}</li>
+                </ul>
+            ))}
+        </>
+    )
 }
 
 export default App
